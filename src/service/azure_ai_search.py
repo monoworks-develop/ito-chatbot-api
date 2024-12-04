cc1cd497526ed414e98007f037c0a10fa7080a81
ito-mono <ito@monoworks.co.jp>
2024-12-04 05:28:48 +0000

    ask時にai_searchからデータを取得するように修正

--- src/service/azure_ai_search.py
+++ src/service/azure_ai_search.py
@@ -4,13 +4,15 @@ from src.infrastructure.azure.ai_search.client import ai_search_client
 
 
 def vector_search(text: str):
+    # MEMO: 対象のIndexにVectorizerが設定されている場合はVectorizableTextQueryを使用できます。
+    #       これを使用することで渡されたtextを自動的にベクトル変換してベクトル検索が行えます。
+    #       これを使用しない場合は自分でベクトルを生成する処理が必要になります。
     vector_query = VectorizableTextQuery(
         text=text,
-        k_nearest_neighbors=5,
         fields="weather_vector,date_vector",
         exhaustive=False,
     )
-
+    # テキストとベクトルのハイブリッド検索
     search_results = ai_search_client.search(
         search_text=text,
         vector_queries=[vector_query],
